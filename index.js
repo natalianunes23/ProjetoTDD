27b29e020a4bb0c31ed54d7a8d7511d25c888ec3
natalianunes23 <nah.nunes23@gmail.com>
2022-06-02 22:14:21 -0300

    projeto com TDD iniciado

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path=require ('path');
+const {engine} = require("express-handlebars");
+const routesAdmin = require('./routes/routesAdmin');
+const app = express();
+app.engine('handlebars',engine({
+    defaultLayout: 'main',
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true,
+    },
+}))
+app.set('view engine', 'handlebars');
+app.use('/',routesAdmin)
+
+app.listen(process.env.port||3000);
+console.log("rodou!")
